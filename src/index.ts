8e805b8b81fe6382e55e2ec85d7fdf7da726471e
Scott Lusk <scottluskcis@github.com>
2025-11-18 10:46:31 -0500

    add skeleton

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import listTeamMembersCommand from './commands/list-team-members.js';
 import codespacesUsageCommand from './commands/codespaces-usage.js';
 import getMigrationExportStatusCommand from './commands/migration-export-status.js';
 import listRepoSecretScanAlertsCommand from './commands/list-repo-secret-scan-alerts.js';
+import listAuditLogEventsCommand from './commands/audit-log-events.js';
 
 const program = createProgram({
   name: 'octokit-sandbox',
@@ -25,6 +26,7 @@ const program = createProgram({
     codespacesUsageCommand,
     getMigrationExportStatusCommand,
     listRepoSecretScanAlertsCommand,
+    listAuditLogEventsCommand,
   ],
 });
 
